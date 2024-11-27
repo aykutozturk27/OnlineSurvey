ecf4b6993ddc1bef147f3f1649247cd388783909
aykutozturk <aykut.ozturk@hotmail.com>
2024-11-27 12:45:02 +0300

    database created

---
+++ OnlineSurvey.DataAccess/Migrations/20241127094131_Init.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineSurvey.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "OperationClaim",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OperationClaim", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                schema: "dbo",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    email = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    password_salt = table.Column<byte[]>(type: "varbinary(500)", nullable: false),
+                    password_hash = table.Column<byte[]>(type: "varbinary(500)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Poll",
+                schema: "dbo",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    title = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    user_id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Poll", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Poll_User_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "dbo",
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserOperationClaims",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    OperationClaimId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserOperationClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserOperationClaims_OperationClaim_OperationClaimId",
+                        column: x => x.OperationClaimId,
+                        principalSchema: "dbo",
+                        principalTable: "OperationClaim",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserOperationClaims_User_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "dbo",
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Option",
+                schema: "dbo",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    poll_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    option_text = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    vote_count = table.Column<int>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Option", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Option_Poll_poll_id",
+                        column: x => x.poll_id,
+                        principalSchema: "dbo",
+                        principalTable: "Poll",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Option_poll_id",
+                schema: "dbo",
+                table: "Option",
+                column: "poll_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Poll_user_id",
+                schema: "dbo",
+                table: "Poll",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserOperationClaims_OperationClaimId",
+                schema: "dbo",
+                table: "UserOperationClaims",
+                column: "OperationClaimId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserOperationClaims_UserId",
+                schema: "dbo",
+                table: "UserOperationClaims",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Option",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "UserOperationClaims",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Poll",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "OperationClaim",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "User",
+                schema: "dbo");
+        }
+    }
+}
